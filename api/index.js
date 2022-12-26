34088bfada2902bbb953c11c277a0c191956b150
Jeetu Bangari <jbangari16@gmail.com>
2022-12-26 13:31:23 +0530

    api

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
